3aa32d62ad256a6fad9a5c0e5df0a7be27ce7e2a
KyleAlbro <kylealbro@charter.net>
2024-04-18 11:05:32 -0600

    pushing changes to test_tracker_user

--- tests/test_tracker_user.php
+++ tests/test_tracker_user.php
@@ -9,7 +9,45 @@
 
 <body>
     <h4>Test Tracker User</h4>
+    <?php
+    require("db_utils.php");
+    $conn = db_connect();
 
+    if ($conn->connect_error == null) {
+        echo "success!";
+    } else {
+        echo "FAILED! " . $conn->connect_error;
+    }
+
+    $user_id = 1;
+
+    $stmt = $conn->prepare("SELECT * FROM tracker_user WHERE user_id = ?");
+    $stmt->bind_param("i", $user_id); 
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if($result->num_rows > 0){
+        echo "<h2>User Information</h2>";
+        echo "<table border='1'>";
+        echo "<tr><th>User ID</th><th>Role ID</th><th>Username</th><th>Password</th></tr>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row["user_id"] . "</td>";
+            echo "<td>" . $row["role_id"] . "</td>";
+            echo "<td>" . $row["username"] . "</td>";
+            echo "<td>" . $row["password"] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "No user found with the provided ID.";
+    }
+
+    $stmt->close();
+    $conn->close();
+        
+
+    ?>
 </body>
 
 </html>
