186b4e980353e565a52c66b9cd7e931bf63b4587
KyleAlbro <kylealbro@charter.net>
2024-04-22 10:56:11 -0600

    adding select all

--- tests/test_courseOffering.php
+++ tests/test_courseOffering.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h4>Test Course Offering</h4>
-    <?php 
+    <?php
     require("../php/courseOffering_ds.php");
 
     $course_obj = new Course_Offering_ds($conn);
@@ -17,14 +19,28 @@
     $singleResult = $course_obj->selectSingle($key);
     $allResult = $course_obj->selectAll($sel_list);
 
+    echo "Testing Single";
     if ($singleResult) {
-        echo "Course ID: " . $singleResult[0]. "<br>";
-        echo "Department ID: " . $singleResult[1]. "<br>";
-        echo "Course Title: " . $singleResult[2]. "<br>";
+        echo "Course ID: " . $singleResult[0] . "<br>";
+        echo "Department ID: " . $singleResult[1] . "<br>";
+        echo "Course Title: " . $singleResult[2] . "<br>";
         echo "<br>";
-    } else {
+    } 
+    else {
         echo "No record found for the given key.";
     }
+
+    echo "Testing Select All";
+    if ($allResult) {
+        foreach ($allResult as $result) {
+            echo "Course ID: " . $result[0] . "<br>";
+            echo "Department ID: " . $result[1] . "<br>";
+            echo "Course Title: " . $result[2] . "<br>";
+            echo "<br>";
+        }
+    }
+
     ?>
 </body>
+
 </html>
