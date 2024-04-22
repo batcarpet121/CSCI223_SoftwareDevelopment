2bca74d684a0e6b2333518ea273b6aa7b6766404
Aaron Polaske <aaron.polaske@gmail.com>
2024-04-22 05:35:36 -0600

    added an insert test function

--- tests/test_textbook.php
+++ tests/test_textbook.php
@@ -23,11 +23,24 @@
     print_r($qryResultMult);
 
     echo '<br><br><br>Select all test<br>';
-    $textbookSelectMult = 'author, course_id';
+    $textbookSelectMult = 'textbook_id, author';
     $qryResultMult = $testTextbook->selectAll($textbookSelectMult);
     print_r($qryResultMult);
 
+    echo '<br><br><br>Insert test<br>';
+    $textbookInsert = array(
+    'class_id' => 1,
+    'title' => 'Insert Test',
+    'author' => 'Aaron Polaske',
+    'isbn' => '0123456789',
+    'publisher' => 'GF Press',
+    'edition' => '3rd',
+    'price' => 79.95);
+    $testTextbook->insert($textbookInsert);
 
+
+    $textbookUpdate = '';
+    $textbookDelete = '';
     
 ?>
 
