98a3e12bd46597d59ad8482729feafd0faf7f67b
Brandon Arensmeyer <barensmeyer@live.com>
2024-04-22 10:19:32 -0600

    test

--- tests/test_course.php
+++ tests/test_course.php
@@ -34,16 +34,14 @@
         echo "No record found for the given key.";
     }
 
-    if ($allResult) {
+    while($allResult) {
         foreach($allresult as $result){
             echo "Course ID: " . $result[0]. "<br>";
             echo "Department ID: " . $result[1]. "<br>";
             echo "Course Title: " . $result[2]. "<br>";
             echo "<br>";
-        }    
-    } else {
-        echo "No record found for the given key.";
-    }
+        }  
+    }  
 
     // if($insertInfo){
     //     echo "Inserted the information";
