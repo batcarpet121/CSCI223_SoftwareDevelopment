efd36ac98fcebc2b926866a7c617a010faec5b75
Brandon Arensmeyer <barensmeyer@live.com>
2024-04-22 10:21:08 -0600

    test

--- tests/test_course.php
+++ tests/test_course.php
@@ -12,36 +12,35 @@
     <?php
     require("../php/course_ds.php");
 
-    
+
     $course_obj = new course_ds($conn);
 
-    
+
     $key = 1;
     // $dept_id=1;
     // $course_title="Javascript";
 
     $singleResult = $course_obj->selectSingle($key);
-    $allResult= $course_obj->selectAll($sel_list);
+    $allResult = $course_obj->selectAll($sel_list);
     // $insertInfo = $course_obj->insert($dept_id, $course_title);
-    
+
 
     if ($singleResult) {
-        echo "Course ID: " . $singleResult[0]. "<br>";
-        echo "Department ID: " . $singleResult[1]. "<br>";
-        echo "Course Title: " . $singleResult[2]. "<br>";
+        echo "Course ID: " . $singleResult[0] . "<br>";
+        echo "Department ID: " . $singleResult[1] . "<br>";
+        echo "Course Title: " . $singleResult[2] . "<br>";
         echo "<br>";
     } else {
         echo "No record found for the given key.";
     }
 
-    while($allResult) {
-        foreach($allresult as $result){
-            echo "Course ID: " . $result[0]. "<br>";
-            echo "Department ID: " . $result[1]. "<br>";
-            echo "Course Title: " . $result[2]. "<br>";
-            echo "<br>";
-        }  
-    }  
+    foreach ($allresult as $result) {
+        echo "Course ID: " . $result[0] . "<br>";
+        echo "Department ID: " . $result[1] . "<br>";
+        echo "Course Title: " . $result[2] . "<br>";
+        echo "<br>";
+    }
+
 
     // if($insertInfo){
     //     echo "Inserted the information";
