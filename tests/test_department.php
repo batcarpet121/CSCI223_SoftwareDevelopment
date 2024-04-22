6c37902018a447c9a0a00cb1f96262f5b7767e3e
Aaron Polaske <aaron.polaske@gmail.com>
2024-04-22 11:30:02 -0600

    changed for dept

---
+++ tests/test_department.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST department Datastore</title>
+</head>
+<body>
+    
+<?php
+    require '../php/department_ds.php';
+
+    $testTextbook = new Textbook_ds();
+
+    echo '<br><br><br>Select single test<br>';
+    $departmentID = 1;
+    $qryResult = $testTextbook->selectSingle($departmentID);   
+    print_r($qryResult);
+
+    echo '<br><br><br>Select all empty test<br>';
+    $departmentSelectMult = '';
+    $qryResultMult = $testDepartment->selectAll($departmentSelectMult);
+    print_r($qryResultMult);
+
+    echo '<br><br><br>Select all test<br>';
+    $departmentSelectField = 'dept_id';
+    $qryResultMult = $testTextbook->selectAll($departmentSelectField);
+    print_r($qryResultMult);
+
+    // echo '<br><br><br>Insert test<br>';
+    // $textbookInsert = array(    
+    // 'class_id' => 1,
+    // 'title' => 'Insert Test',
+    // 'author' => 'Aaron Polaske',
+    // 'isbn' => '0123456789',
+    // 'publisher' => 'GF Press',
+    // 'edition' => '3rd',
+    // 'price' => 79.95);
+
+    $classID = 1;
+    $title = 'Insert Test';
+    $author = 'Aaron Polaske';
+    $isbn = '0123456789';
+    $publisher = 'GF Press';
+    $edition = 'Edition';
+    $price = 79.95;
+
+
+    $testTextbook->insert($classID, $title, $author, $isbn, $publisher, $edition, $price);
+
+
+    // echo '<br><br><br>Update test<br>';
+    // $textbookUpdate = [
+    // 'class_id' => 1,
+    // 'title' => 'Developing Software',
+    // 'author' => 'David Thomas',
+    // 'isbn' => '978-1-489-03264-9',
+    // 'publisher' => 'GFCMSU',
+    // 'edition' => '1st Edition',
+    // 'price' => 64.00,
+    // 'textbook_id' => 1];
+    // $testTextbook->update($textbookUpdate);
+    
+
+
+
+    // $textbookDelete = '';
+    
+?>
+
+</body>
+</html>
