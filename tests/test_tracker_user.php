932fb02ec1c9edeb9ae3f9cf4314e60e577bc9c4
KyleAlbro <kylealbro@charter.net>
2024-04-23 14:49:26 -0600

    testing insert

--- tests/test_tracker_user.php
+++ tests/test_tracker_user.php
@@ -18,15 +18,15 @@
     //Select Single values
     $key = 1;
 
-    //Insert values
-    $user_id = 2;
-    $role_id = 2;
-    $username = "TestUser2";
-    $password = "TestPassword2";
+    // //Insert values
+    // $user_id = 2;
+    // $role_id = 2;
+    // $username = "TestUser2";
+    // $password = "TestPassword2";
 
     $singleResult = $tracker_user_obj->selectSingle($key);
     $allResult = $tracker_user_obj->selectAll($sel_list);
-    $insertInfo = $tracker_user_obj->insert($user_id, $role_id, $username, $password);
+    //$insertInfo = $tracker_user_obj->insert($user_id, $role_id, $username, $password);
 
     echo "Testing select single <br>";
 
@@ -52,11 +52,11 @@
         }
     }
 
-    if($insertInfo){
-        echo "Insert successful!";
-    }else{
-        echo "Insert Failed.";
-    }
+    // if($insertInfo){
+    //     echo "Insert successful!";
+    // }else{
+    //     echo "Insert Failed.";
+    // }
 
     ?>
 
