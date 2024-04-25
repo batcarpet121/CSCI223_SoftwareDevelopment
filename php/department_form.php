aaa6a618ab500d5ed2531ab4cd7f9ca95496ad90
Brandon Arensmeyer <barensmeyer@live.com>
2024-04-25 11:27:34 -0600

    test

--- php/department_form.php
+++ php/department_form.php
@@ -63,8 +63,8 @@
                         <!-- <input type="checkbox" id="addMultipleCourses"> -->
                     </div>
                     <div id="ADD_DEPT" class="addForm">
-                        <?php if ($_SERVER["REQUEST_METHOD"] == "POST") { ?>
-                        <form action="department_ds.php" method="POST">
+                        <?php if ($_SERVER["REQUEST_METHOD"] == "GET") { ?>
+                        <form action="<?php $_SERVER['department_ds.php']?>" method="POST">
                             <label for="Dept_Title">Enter Department Title:</label>
                             <input type="text" id="Dept_Title" name="Dept_Title" placeholder="Enter the Department Title" required>
                             <div class="SubmitButton">
@@ -72,8 +72,7 @@
                             </div>
                         </form>
                         <?php 
-                            $deptTitle = trim($_POST["Dept_Title"]);
-                            $insert = $department->insert($deptTitle);
+                            
                         } ?>
                     </div>
 
