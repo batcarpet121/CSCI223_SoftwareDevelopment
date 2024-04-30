98ad5b18bf354b64bfdc5fc8caf6fd55a0a1f485
Aaron Polaske <aaron.polaske@gmail.com>
2024-04-30 09:21:53 -0600

    awd

--- php/textbook_ds.php
+++ php/textbook_ds.php
@@ -183,7 +183,7 @@ class Textbook_ds extends Textbook {
     }
 
     public function getCourseOfferings() {
-        $qry = "SELECT CO.course_offering_id, CO.term, CO.year
+        $qry = "SELECT CO.course_offering_id
         FROM CourseOffering CO
         INNER JOIN Textbook T ON CO.course_offering_id = T.course_offering_id";
         $stmt = $this->conn->prepare($qry);
@@ -197,12 +197,10 @@ class Textbook_ds extends Textbook {
         $results = [];
         $rowCount = 0;
         if ($stmt->num_rows > 0) {
-            $stmt->bind_result($course_offering_id, $term, $year);
+            $stmt->bind_result($course_offering_id);
             while ($stmt->fetch()) {
                 $results[] = [
-                    'course_offering_id' => $course_offering_id,
-                    'term' => $term,
-                    'year' => $year
+                    'course_offering_id' => $course_offering_id
                 ];
                 $rowCount++;
             }
