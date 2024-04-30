1794d5b2bb3ba7e85274fa6987046829364de4a1
Aaron Polaske <aaron.polaske@gmail.com>
2024-04-30 09:29:17 -0600

    I bet I know what the issue is now

--- php/textbook_ds.php
+++ php/textbook_ds.php
@@ -183,34 +183,32 @@ class Textbook_ds extends Textbook {
     }
 
     public function getCourseOfferings() {
-        $qry = "SELECT CO.course_offering_id
+        $qry = "SELECT CO.course_offering_id, CO.term, CO.year
         FROM CourseOffering CO
         INNER JOIN Textbook T ON CO.course_offering_id = T.course_offering_id";
         $stmt = $this->conn->prepare($qry);
-        // if (!$stmt) {
-        //     echo "Prepare error: " . $this->conn->error;
-        //     return [];
-        // }
+        if (!$stmt) {
+            echo "Prepare error: " . $this->conn->error;
+            return [];
+        }
         $stmt->execute();
         $stmt->store_result();
 
         $results = [];
         $rowCount = 0;
         if ($stmt->num_rows > 0) {
-            $stmt->bind_result($course_offering_id);
+            $stmt->bind_result($course_offering_id, $term, $year);
             while ($stmt->fetch()) {
                 $results[] = [
-                    'course_offering_id' => $course_offering_id
+                    'course_offering_id' => $course_offering_id,
+                    'term' => $term,
+                    'year' => $year
                 ];
                 $rowCount++;
             }
         }
         
-        if ($rowCount > 0) {
-            return $results;
-        } else {
-            echo "Nothing to return";
-        }
+      return $results;
     }
 
     public function __destruct() {
