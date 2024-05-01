b1b10d11b391578e815fb52da56633d45d39a13c
Aaron Polaske <aaron.polaske@gmail.com>
2024-05-01 08:42:34 -0600

    basic user forms created

---
+++ utils/user_account_utils.php
@@ -0,0 +1,122 @@
+<?php
+
+class User {
+
+    var $role_id;
+    var $username;
+    var $password;
+
+}
+
+require('../utils/db_utils.php');
+class User_Account_Utils extends User {
+
+    public $conn;
+
+    public function __construct()
+    {
+        $this->conn = db_connect();
+        
+        if ($this->conn->connect_error == null) {
+            echo "success!";
+        } else {
+            echo "FAILED! " . $this->conn->connect_error;
+        }
+        
+    }
+
+    public function selectSingle($key) {
+        if ($key == null) {
+            return false;
+        }
+        $qry = 'SELECT * FROM Tracker_User WHERE username = ?';
+        $stmt = '';
+        $stmt = $this->conn->prepare($qry);
+        $stmt->bind_param('s', $key);
+        $stmt->execute();
+        $stmt->bind_result(
+            $this->username,
+            $this->password);
+        
+        $row = array();
+        if ($stmt->fetch()) {
+            array_push($row, $this->username);
+            array_push($row, $this->password);
+        }
+        if (!empty($row)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public function insert($values) {
+        if ($values == null){
+            return false;
+        }
+    
+        $qry = "INSERT INTO Tracker_User (role_id, username, password) VALUES (?, ?, ?)";
+        $stmt = $this->conn->prepare($qry);
+
+        $stmt->bind_param(
+            'iss',
+            $values['role_id'],
+            $values['username'], 
+            $values['password']);
+
+        $success = $stmt->execute();
+        if (!$success) {
+            echo "Insert failed: " . $stmt->error;
+        } else {
+            echo "Insert Successful!";
+        }
+    }
+
+
+
+    public function update($row) {
+        if ($row == null) {
+            return false;
+        }
+
+        $qry = 'UPDATE Tracker_User SET role_id = (?), username = (?), password = (?) 
+                WHERE username = (?)';
+
+        $stmt = $this->conn->prepare($qry);
+        $stmt->bind_param('isss', $row['role_id'], $row['username'], $row['password'], $row['username']);
+
+        $success = $stmt->execute();
+        if (!$success) {
+            echo "Update failed: " . $stmt->error;
+        } else {
+            echo "Update Successful!";
+        }
+    }
+
+    
+    public function delete($user){
+        if ($user == null) {
+            return false;
+        }
+
+        $qry = 'DELETE FROM Tracker_User WHERE username = ?';
+        $stmt = $this->conn->prepare($qry);
+        $stmt->bind_param('s', $user);
+
+        $success = $stmt->execute();
+        if (!$success) {
+            echo "Delete failed: " . $stmt->error;
+        } else {
+            echo "Delete Successful!";
+        }
+    }
+
+    public function __destruct() {
+        $this->conn->close();
+    }
+
+}
+
+
+?>
