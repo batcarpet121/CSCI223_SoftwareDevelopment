3ff1ece4863110b4ad58b861b0e88e104394943e
Steve Robinett <steven.robinett@gfcmsu.edu>
2024-05-08 22:36:10 -0600

    test api page

---
+++ tests/test_api.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API Test</title>
+</head>
+
+<body>
+
+    <h4>Api Test</h4>
+    <form action="../php/api.php" method="get" >
+        
+    <input type="submit">
+    </form>
+
+
+
+</body>
+
+</html>
